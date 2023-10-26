1c309efa89f6d28738b84672db3f42af1b8d989e
Skyler Woolf <skylerwoolf@gmail.com>
2023-10-26 16:52:03 -0600

    added login functionality to index.html

--- tasks.js
+++ tasks.js
@@ -1,41 +1,23 @@
 function newCard() {
-    // Create a new card element
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.style.width = '25rem';
-  
-    // Create a card body element
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-  
-    // Append the card body element to the card element
-    card.appendChild(cardBody);
-  
-    // Append the card element to the cards container element
-    const cardsContainer = document.querySelector('.cards_container');
-    cardsContainer.appendChild(card);
-  }
-
-function newCard() {
-    // Get the first card element
+    // grab first card element
     const firstCard = document.querySelector('.cards_container .card');
   
-    // Create a new card element
+    // new card
     const card = document.createElement('div');
     card.classList.add('card');
     card.style.width = '25rem';
   
-    // Create a card body element
+    // card body
     const cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
   
-    // Copy the text from the first card to the new card
+    // copy first
     cardBody.innerHTML = firstCard.querySelector('.card-body').innerHTML;
   
-    // Append the card body element to the card element
+    // append
     card.appendChild(cardBody);
   
-    // Append the card element to the cards container element
+    // append to container
     const cardsContainer = document.querySelector('.cards_container');
     cardsContainer.appendChild(card);
   }
