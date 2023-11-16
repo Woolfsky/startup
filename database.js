74bc3fb5a74938d966341a55e0645de0d8d71053
Skyler Woolf <skylerwoolf@gmail.com>
2023-11-16 14:48:17 -0700

    Create outline of db changes

---
+++ database.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const scoreCollection = db.collection('cards');
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+async function getDict() {
+    const d =  await db.find();
+    return d;
+}
+
+async function updateDict(k, v) {
+    find = 
+    value = 
+    db.updateOne(find, value);
+}
+
+
+module.exports = { getDict, updateDict };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -7,25 +8,37 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
-// Serve up the frontend static content hosting
+// Serve up the applications static content hosting
 app.use(express.static('public'));
 
 // Router for service endpoints
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
-
 // Get Web Storage Dictionary
-apiRouter.get('/getDictionary', (_req, res) => {
-    res.json(webStorage);
+// apiRouter.get('/getDictionary', (_req, res) => {
+//     res.json(webStorage);
+// });
+apiRouter.get('/getDictionary', async (_req, res) => {
+  const _dict = await DB.getDict();
+  res.json(_dict);
 });
 
 // Update Dictionary
-apiRouter.post('/updateDictionary', (req, res) => {
-    const key = req.body.key;
-    const value = req.body.value;
-    webStorage[key] = value;
-    res.json(webStorage);
+// apiRouter.post('/updateDictionary', (req, res) => {
+//     const key = req.body.key;
+//     const value = req.body.value;
+//     webStorage[key] = value;
+//     res.json(webStorage);
+// });
+apiRouter.post('/updateDictionary', async (req, res) => {
+  const key = req.body.key;
+  const value = req.body.value;
+  DB.updateDict(key, value);
+  
+
+  webStorage[key] = value;
+  res.json(webStorage);
 });
 
 
@@ -41,4 +54,4 @@ app.listen(port, () => {
 
 
 
-let webStorage = {};
+// let webStorage = {};
