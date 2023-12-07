1cfb032c1655d1f441bb3304bf65d57632f20d3d
Skyler Woolf <skylerwoolf@gmail.com>
2023-12-06 20:01:29 -0700

    Created App component

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <div className="body" >
+        {/* Header */}
+        <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+            <h1 className="focusH1" >focus</h1>
+
+            {/* Menu */}
+            <ul className="nav justify-content-end">
+                <li className="nav-item"><a className="nav-link" aria-current="page" href="tasks.html">Tasks</a></li>
+                <li className="nav-item"><a className="nav-link" href="sessions.html">Sessions</a></li>
+                <li className="nav-item"><a className="nav-link" href="community.html">Community</a></li>
+            </ul>
+
+            {/* Login */}
+            <div className="col-md-1 text-center">
+                <button id="logout_button" type="button" className="btn btn-outline-secondary" >Logout</button>
+            {/* this button ^^ is missing this code: onClick='logout()' */}
+            </div>
+
+            <hr />
+        </header>
+
+        <main>App components go here</main>
+
+        {/* Footer */}
+        <footer>
+            <span>Creator's GitHub can be found</span> <a href="https://github.com/Woolfsky/startup/tree/main">here</a>
+        </footer>
+    </div>
+  )
 }
